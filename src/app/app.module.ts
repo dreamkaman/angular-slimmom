44c5fdd00e17e49024b3d3a9ebf2f69de9bd7550
Dreamkaman <riabushko@gmail.com>
2022-11-18 00:10:42 +0200

    Add sprite, logo, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomePageComponent } from './features/home-page/home-page.component';
+import { CalculatorPageComponent } from './features/calculator-page/calculator-page.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CalculatorPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
